d645697e81c37cf6398c437953029a80d036da7d
Isaac Pompa <isaacsalvador18@gmail.com>
2015-12-10 15:52:36 -0600

    LogOut test API
    * Spec for logout in API
    * Change to Faker name in User spec name attribute

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    name { Faker::Internet.name }
+    name { Faker::Name.name }
     password { Faker::Internet.password(8) }
   end
 end
