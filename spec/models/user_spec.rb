29ed5057529ca352d00f977d9be0b099f4956f1c
lruiz05 <luis05021990@gmail.com>
2015-12-16 11:51:14 -0600

    Adds association model institution in spec user
    * Adds association between models user and institution in user factory
    * Adds validation belong to institution in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   shared_examples 'a valid user' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:name) }
+    it { should have_db_index(:institution_id) }
+    it { should belong_to(:institution) }
 
     it 'should be valid' do
       expect(user).to be_valid
@@ -38,6 +40,11 @@ RSpec.describe User, type: :model do
 
   context 'with a nil email' do
     let(:user) { build(:user, email: '') }
+    it_behaves_like 'an invalid user'
+  end
+
+  context 'with a nil institution' do
+    let(:user) { build(:user, institution: nil) }
     it_behaves_like 'an invalid user'
   end
 end
