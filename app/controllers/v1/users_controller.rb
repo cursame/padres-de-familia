11055659e83ee0087292657ed9d64500f4740f70
lruiz05 <luis05021990@gmail.com>
2016-01-05 10:12:54 -0600

    Adds users controller
    * CRUD operations

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,41 @@
+class V1::UsersController < ApplicationController
+  before_action :authenticate_v1_user!
+  respond_to :json
+
+  def show
+    user = User.find params[:id]
+    render json: user, status: 200, location: [:v1, user]
+  end
+
+  def create
+    user = User.new(user_params)
+    password = Devise.friendly_token.first(8)
+    user.password = password
+    if user.save
+      render json: user, status: 201, location: [:v1, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = User.find params[:id]
+    if user.update(user_params)
+      render json: user, status: 200, location: [:v1, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :institution_id)
+    end
+end
